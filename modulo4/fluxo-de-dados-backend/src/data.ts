c097c728de0a225d9b9ccf4ba2ac49ea52edf482
gutto-moura <gmourah1792@gmail.com>
2022-01-26 19:41:09 -0300

    Fluxo de dados Backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,13 @@
+export type infoProducts = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const dataProducts:infoProducts[] = [
+    {id: '123654', name:"Arroz", price:30},
+    {id: '321654', name:"Feijão", price:50},
+    {id: '789523', name:"Açucar", price:15},
+    {id: '325987', name:"Café", price:26},
+    {id: '454567', name:"Farinha", price:81}
+]
